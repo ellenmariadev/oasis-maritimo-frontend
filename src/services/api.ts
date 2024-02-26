846ec3552c21747135bf452fcece541d343484e7
ellenmariadev <ellenmariadev@gmail.com>
2024-02-26 12:51:02 -0300

    fix: base url default + next eslint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,8 @@
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL ??
-  "https://oasis-maritimo-afe4c920a559.herokuapp.com/api/v1";
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://oasis-maritimo-afe4c920a559.herokuapp.com/api/v1";
+
+export async function getCheckConnection() {
+  const result = await fetch(BASE_URL);
+  const connection = await result.json();
+  console.log(connection);
+  return connection;
+}
