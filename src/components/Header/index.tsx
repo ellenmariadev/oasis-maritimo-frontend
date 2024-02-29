adcceaa5a1856c58cf07839672fb846d679849ff
ellenmariadev <ellenmariadev@gmail.com>
2024-02-29 02:35:13 -0300

    feat(component): update header/sidebar styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,24 +3,51 @@
 import { Avatar, Icon, Link, List, WrapItem } from "@chakra-ui/react";
 import { SlLogout } from "react-icons/sl";
 import styles from "./styles.module.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
 
 const Header = () => {
+
+  const [login, setLogin] = useState("");
+  const [role, setRole] = useState("");
+
+  useEffect(() => {
+    setLogin(localStorage.getItem("@login") ?? "");
+    setRole(localStorage.getItem("@role") ?? "");
+  }, []);
+
+  const pathname = usePathname();
+
+  const logout = () => {
+    localStorage.removeItem("@login");
+    localStorage.removeItem("@token");
+    localStorage.removeItem("@role");
+    setLogin("");
+    setRole("");
+  };
+
+  const router = useRouter();
+
+  const goBack = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    router.back();
+  };
+
   return (
     <header className={styles.header}>
-      <nav>
-        <Link href="/">Voltar</Link>
+      <nav style={{ placeContent: pathname === "/" ? "end" : "none" }}>
+        {pathname !== "/" && <Link href="#" onClick={goBack}>Voltar</Link>}
         <List>
           <WrapItem gap={2}>
             <Avatar
               size='xs'
-              name='ADMIN'
+              name={role}
             />
-              administrador
+            {login}
           </WrapItem>
           <WrapItem gap={2} placeItems={"center"}>
             <Icon as={SlLogout} />
-            <Link href="/auth/login">Sair</Link>
+            <Link href="/auth/login" onClick={logout}>Sair</Link>
           </WrapItem>
         </List>
       </nav>
